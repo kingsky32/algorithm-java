6dac6c64b0cdb2671000f7f5fcb444b277fc6500
seungju <kingsky32@gmail.com>
2020-08-12 03:30:09 +0900

    Recursion

---
+++ src/Recursion/Code03.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+public class Code03 {
+	
+	public static void main(String [] args) {
+		int result = func(4);
+		System.out.println(result);
+	}
+	
+	public static int func(int n) {
+		if (n == 0)
+			return 0;
+		else 
+			return n + func(n - 1);
+	}
+	
+}
