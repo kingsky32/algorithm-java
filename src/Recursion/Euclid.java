fb93dd6f2a74e5d899d63b9d5af0f6e9f0045571
seungju <kingsky32@gmail.com>
2020-08-12 03:56:51 +0900

    Recursion

---
+++ src/Recursion/Euclid.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+public class Euclid {
+	
+	public static void main(String [] args) {
+		int result = gcd(4, 4);
+		System.out.println(result);
+	}
+	
+	public static int gcd(int m, int n) {
+		if (m < n) {
+			int temp = m;
+			m = n;
+			n = temp;
+		}
+		if (m % n == 0) 
+			return n;
+		else 
+			return gcd(n, m % n);
+	}
+	
+}
---
+++ src/Recursion/Power.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+public class Power {
+	
+	public static void main(String [] args) {
+		int result = power(4, 4);
+		System.out.println(result);
+	}
+	
+	public static int power(int x, int n) {
+		if (n == 0)
+			return 1;
+		else 
+			return x * power(x, n - 1);
+	}
+	
+}
