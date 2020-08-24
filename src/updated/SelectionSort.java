6c352587d5714b9178b6d46aa11f3a27a71a0ed1
seungju <kingsky32@gmail.com>
2020-08-24 20:59:26 +0900

    Cleanup

--- src/updated/SelectionSort.java
+++ src/updated/SelectionSort.java
@@ -3,13 +3,12 @@ package updated;
 public class SelectionSort {
 	
 	public static void selectionSort (int[] arr) {
-		int size = arr.length;
 		int min;
 		int temp;
 		
-		for(int i=0; i<size-1; i++) {
+		for(int i = 0; i < arr.length - 1; i++) {
             min = i;
-            for(int j=i+1; j<size; j++) {
+            for(int j = i + 1; j < arr.length; j++) {
                 if(arr[min] > arr[j]) {
                     min = j;
                 }
