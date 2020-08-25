97db59c4e38aa3d5922cc3753cb3b6fdce2f4ee6
seungju <kingsky32@gmail.com>
2020-08-25 11:02:27 +0900

    cleanup

--- src/updated/InsertionSort.java
+++ src/updated/InsertionSort.java
@@ -30,7 +30,7 @@ public class InsertionSort {
 		
 		insertionSort(arr);
 		
-		for(int i = 0; i < arr.length; i++){
+		for(int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
         }
 	}
